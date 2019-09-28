edc9547ae68f71a38578a2e6359fe8e0a653423b
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-09-28 20:49:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,10 @@
 def square_array(array)
   counter = 0
-  
+
   while array[counter] do
     squared_element=[array[counter]*array[counter]]
     new_array.insert(counter, squared_element)
+
     counter += 1
   end
 end
